4219c5b49e00fd9d3758de4f5acd8f0ed4a7d88e
John Ryn Salazar <159772476+10awpt05@users.noreply.github.com>
2025-08-20 16:57:47 +0800

    Update FirebaseService.php

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -12,26 +12,26 @@ class FirebaseService
     protected $auth;
     protected $database;
 
-    public function __construct()
+      public function __construct()
     {
-        $credentials = config('firebase.credentials');
         $databaseUrl = config('firebase.database_url');
 
-        $factory = (new Factory)
-    ->withServiceAccount([
-        "type" => "service_account",
-        "project_id" => env('FIREBASE_PROJECT_ID'),
-        "private_key_id" => env('FIREBASE_PRIVATE_KEY_ID'),
-        "private_key" => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
-        "client_email" => env('FIREBASE_CLIENT_EMAIL'),
-        "client_id" => env('FIREBASE_CLIENT_ID'),
-        "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
-        "token_uri" => "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url" => env('FIREBASE_CLIENT_CERT_URL'),
-    ])
-    ->withDatabaseUri($databaseUrl);
+        $serviceAccount = [
+            "type" => "service_account",
+            "project_id" => env('FIREBASE_PROJECT_ID'),
+            "private_key_id" => env('FIREBASE_PRIVATE_KEY_ID'),
+            "private_key" => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
+            "client_email" => env('FIREBASE_CLIENT_EMAIL'),
+            "client_id" => env('FIREBASE_CLIENT_ID'),
+            "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
+            "token_uri" => "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url" => env('FIREBASE_CLIENT_CERT_URL'),
+        ];
 
+        $factory = (new Factory)
+            ->withServiceAccount($serviceAccount)
+            ->withDatabaseUri($databaseUrl);
 
         $this->auth = $factory->createAuth();
         $this->database = $factory->createDatabase();
