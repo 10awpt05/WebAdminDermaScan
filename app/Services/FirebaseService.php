90b0955e02de2a77f613502211eac0b81932c9a5
John Ryn Salazar <159772476+10awpt05@users.noreply.github.com>
2025-08-20 16:25:04 +0800

    Update FirebaseService.php

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -14,26 +14,19 @@ class FirebaseService
 
     public function __construct()
     {
-        // Build service account from env
-        $serviceAccount = [
-            'type' => 'service_account',
-            'project_id' => env('FIREBASE_PROJECT_ID'),
-            'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'), // optional
-            'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
-            'client_email' => env('FIREBASE_CLIENT_EMAIL'),
-            'client_id' => env('FIREBASE_CLIENT_ID'), // optional
-            'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
-            'token_uri' => 'https://oauth2.googleapis.com/token',
-            'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
-            'client_x509_cert_url' => env('FIREBASE_CLIENT_X509_CERT_URL') // optional
-        ];
+        $credentials = config('firebase.credentials');
+        $databaseUrl = config('firebase.database_url');
 
         $factory = (new Factory)
-            ->withServiceAccount($serviceAccount)
-            ->withDatabaseUri(env('FIREBASE_DATABASE_URL'));
+            ->withServiceAccount($credentials)
+            ->withDatabaseUri($databaseUrl);
 
-        $this->auth = $factory->createAuth();
         $this->database = $factory->createDatabase();
+    }
+
+    public function getDatabase()
+    {
+        return $this->database;
     }
 
     /** ---------------- AUTH ---------------- */
