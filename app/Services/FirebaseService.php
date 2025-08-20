e8643331bc6aed9e516037c1564698b58308dc9b
John Ryn Salazar <159772476+10awpt05@users.noreply.github.com>
2025-08-20 14:46:45 +0800

    Update FirebaseService.php

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -9,16 +9,23 @@ use Illuminate\Support\Facades\Log;
 
 class FirebaseService
 {
-    protected $auth;
+     protected $auth;
     protected $database;
 
     public function __construct()
     {
+        // Build service account from env
         $serviceAccount = [
             'type' => 'service_account',
             'project_id' => env('FIREBASE_PROJECT_ID'),
-            'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+            'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'), // optional
             'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
+            'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+            'client_id' => env('FIREBASE_CLIENT_ID'), // optional
+            'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
+            'token_uri' => 'https://oauth2.googleapis.com/token',
+            'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
+            'client_x509_cert_url' => env('FIREBASE_CLIENT_X509_CERT_URL') // optional
         ];
 
         $factory = (new Factory)
