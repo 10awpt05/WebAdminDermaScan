4b9f53a2b4e9d2b3f32f6dce4f0e3b3c2c256ab9
John Ryn Salazar <159772476+10awpt05@users.noreply.github.com>
2025-08-25 15:57:31 +0800

    Update firebase.php

--- config/firebase.php
+++ config/firebase.php
@@ -3,6 +3,9 @@
 use Kreait\Firebase\Factory;
 
 return [
-    'credentials' => env('FIREBASE_CREDENTIALS', '/etc/secrets/firebase.json'),
+
+    'credentials' => env('FIREBASE_CREDENTIALS', base_path('firebase.json')),
+
     'database_url' => env('FIREBASE_DATABASE_URL'),
 ];
+
