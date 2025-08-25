b04faac56b2a835508de49dff4bd9ef9d7247177
John Ryn Salazar <159772476+10awpt05@users.noreply.github.com>
2025-08-25 18:13:08 +0800

    Update firebase.php

--- config/firebase.php
+++ config/firebase.php
@@ -2,10 +2,31 @@
 
 use Kreait\Firebase\Factory;
 
+<?php
+
 return [
 
-    'credentials' => env('FIREBASE_CREDENTIALS', base_path('firebase.json')),
+    'default' => env('FIREBASE_PROJECT_ID'),
 
-    'database_url' => env('FIREBASE_DATABASE_URL'),
+    'projects' => [
+        env('FIREBASE_PROJECT_ID') => [
+            'credentials' => [
+                'type'                        => 'service_account',
+                'project_id'                  => env('FIREBASE_PROJECT_ID'),
+                'private_key_id'              => env('FIREBASE_PRIVATE_KEY_ID'),
+                'private_key'                 => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')), // 🔑 fix line breaks
+                'client_email'                => env('FIREBASE_CLIENT_EMAIL'),
+                'client_id'                   => env('FIREBASE_CLIENT_ID'),
+                'auth_uri'                    => 'https://accounts.google.com/o/oauth2/auth',
+                'token_uri'                   => 'https://oauth2.googleapis.com/token',
+                'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
+                'client_x509_cert_url'        => env('FIREBASE_CLIENT_CERT_URL'),
+            ],
+            'database' => [
+                'url' => env('FIREBASE_DATABASE_URL'),
+            ],
+        ],
+    ],
+
 ];
 
