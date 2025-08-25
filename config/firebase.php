14d28a04594fc97d321168defee9c809bb348a0b
John Ryn Salazar <159772476+10awpt05@users.noreply.github.com>
2025-08-25 14:59:40 +0800

    Update firebase.php

--- config/firebase.php
+++ config/firebase.php
@@ -4,12 +4,9 @@ use Kreait\Firebase\Factory;
 
 return [
 
-    'credentials' => [
-        'type' => 'service_account',
-        'project_id' => env('FIREBASE_PROJECT_ID'),
-        'client_email' => env('FIREBASE_CLIENT_EMAIL'),
-        'private_key' => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
-    ],
+    'credentials' => storage_path('app/firebase.json'), // if you upload manually into storage
+    // OR if using Render Secret Files:
+    'credentials' => '/etc/secrets/firebase.json',
 
     'database_url' => env('FIREBASE_DATABASE_URL'),
 ];
